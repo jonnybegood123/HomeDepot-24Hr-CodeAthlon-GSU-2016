bc468d85df1eb46a97b3f5ac20b1d99625c0989b
Jonathan Robins <jonrobns@gmail.com>
2016-02-20 10:25:07 -0500

    c hangers

--- home_depot_connect/routes/index.js
+++ home_depot_connect/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home Depot Connect' });
 });
 
+router.get('/categories', function(req, res) {
+  res.send('Specific Categories');
+});
+
 module.exports = router;
