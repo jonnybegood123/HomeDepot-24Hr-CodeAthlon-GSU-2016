f8b3afa09db12aef459a04e96a3677e0df51f4b6
Abhinav Reddy <reddy@Abhinavs-MacBook-Pro.local>
2016-02-20 03:17:58 -0500

    Changes with routing and shit

--- home_depot_connect/routes/index.js
+++ home_depot_connect/routes/index.js
@@ -6,4 +6,37 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home Depot Connect' });
 });
 
+router.post('/signin', function(req, res) {
+            res.redirect("login");
+});
+
+/* GET login page. */
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Home Depot Connect' });
+});
+
+router.post('/createacc', function(req, res) {
+            res.redirect("account");
+});
+
+/* GET Account page. */
+router.get('/account', function(req, res) {
+  res.render('account', { title: 'Home Depot Connect' });
+});
+//
+// /* GET pro page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Home Depot Connect' });
+// });
+//
+// /* GET options page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Home Depot Connect' });
+// });
+//
+// /* GET list page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Home Depot Connect' });
+// });
+
 module.exports = router;
